107b490ecbe56a58348265f2c73b655873fead54
biteleven <sigalas.kostas@gmail.com>
2014-08-25 17:11:31 +0300

    u

--- www/scripts/airlinedata.js
+++ www/scripts/airlinedata.js
@@ -4,7 +4,7 @@ var airlinesData = function(timeToLoad) {
 
 airlinesData.prototype = function() {
 	var ffInfo = {
-		firstName: 'Mike', lastName: 'Palikaras', ffNum: '12345678', status: 'Diamond', miles: 55555,
+		firstName: 'Kostas', lastName: 'Palikaras', ffNum: '12345678', status: 'Diamond', miles: 55555,
     
 		flights: [
 			{
